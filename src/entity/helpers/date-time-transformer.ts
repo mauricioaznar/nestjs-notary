3dcb2ac241dce0d67deedc30bdce871f466e460f
Mauricio Aznar <mauricioaznar94@gmail.com>
2021-05-21 21:24:46 -0500

    initial commit

--- src/entity/helpers/date-time-transformer.ts
+++ src/entity/helpers/date-time-transformer.ts
@@ -2,6 +2,9 @@ import * as moment from 'moment';
 
 export const dateTimeTransformer = {
   to: (value: string) => value,
-  from: (value: string) =>
-    moment(value, 'YYYY-MM-DD HH:mm:ss').format('YYYY-MM-DD HH:mm:ss'),
+  from: (value: string) => {
+    return moment(value, 'YYYY-MM-DD HH:mm:ss').isValid()
+      ? moment(value, 'YYYY-MM-DD HH:mm:ss').format('YYYY-MM-DD HH:mm:ss')
+      : null;
+  },
 };
